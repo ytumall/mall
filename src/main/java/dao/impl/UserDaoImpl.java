b684672b3f1fcc2106e0274f888eba33c4bbaa45
jiweicheng666 <1935672695@qq.com>
2022-11-09 10:54:19 +0800

    这里写该操作的备注信息

---
+++ src/main/java/dao/impl/UserDaoImpl.java
@@ -0,0 +1,16 @@
+package dao.impl;
+
+import dao.UserDao;
+
+/**
+ * @Description: UserDaoImpl
+ * @Author: ygh
+ * @Date: 2022/11/9 10:15
+ */
+public class UserDaoImpl implements UserDao {
+
+    @Override
+    public void save() {
+        System.out.println("保存....");
+    }
+}
